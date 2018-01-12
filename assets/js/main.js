ddd63828a77a304adff60a90d2981d88f3ee7772
JacieWDI <janischan@hotmail.com>
2018-01-12 18:41:26 +0000

    general layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 (function($) {
 
 	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
+	   xlarge: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
 		small: '(max-width: 736px)',
